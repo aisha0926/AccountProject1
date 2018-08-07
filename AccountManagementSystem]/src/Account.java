31757efaa74d16c1588cd20c0ddc72875a39e9ab
aisha0926 <aisha0926@hotmail.com>
2018-08-07 17:20:41 +0100

    adding

---
+++ AccountManagementSystem]/src/Account.java
@@ -0,0 +1,12 @@
+
+public class Account {
+	private String fName;
+	private String lName;
+	private String accNumber;
+	
+	public Account(String firstName, String lastName, String accountNumber) {
+		this.fName = firstName;
+		this.lName = lastName;
+		this.accNumber = accountNumber;
+	}
+}
---
+++ AccountManagementSystem]/src/ServiceClass.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class ServiceClass {
+
+	public static void main(String args[]) {
+		Map<Object, String> accounts = new HashMap<Object, String>();
+		
+		Account acc = new Account("Test", "This","12341");
+		Account acc2 = new Account("NUmber2", "NM2","8583");
+		accounts.put(acc, "Name");
+		accounts.put(acc2, "Number3");
+		
+		System.out.println(accounts.size());
+		
+
+	}
+	
+}
